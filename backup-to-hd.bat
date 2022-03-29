0f9d407a1ff44b2310e28b270633e8f30023f3ae
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2022-03-29 17:51:24 -0300

    Created tools backup script.

--- backup-to-hd.bat
+++ backup-to-hd.bat
@@ -31,44 +31,50 @@ set path[15]=AppData\Local\Microsoft\Credentials
 set path[16]=AppData\Local\Microsoft\Windows\Cursors
 set path[17]=AppData\Local\Microsoft\Windows\Fonts
 set path[18]=AppData\Local\oh-my-posh
-set path[19]=AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
-set path[20]=AppData\Local\qBittorrent
-set path[21]=AppData\Local\TeamViewer
-set path[22]=AppData\Roaming\Apowersoft\GitMind
-set path[23]=AppData\Roaming\Code
-set path[24]=AppData\Roaming\DBeaverData
-set path[25]=AppData\Roaming\Docker
-set path[26]=AppData\Roaming\Docker Desktop
-set path[27]=AppData\Roaming\espanso
-set path[28]=AppData\Roaming\flameshot
-set path[29]=AppData\Roaming\FileZilla
-set path[30]=AppData\Roaming\Google
-set path[31]=AppData\Roaming\Insomnia
-set path[32]=AppData\Roaming\JAM Software
-set path[33]=AppData\Roaming\LGHUB
-set path[34]=AppData\Roaming\Logishrd
-set path[35]=AppData\Roaming\Macro Deck
-set path[36]=AppData\Roaming\Microsoft\Windows\PowerShell
-set path[37]=AppData\Roaming\NetBeans
-set path[38]=AppData\Roaming\obs-studio
-set path[39]=AppData\Roaming\PLSQL Developer
-set path[40]=AppData\Roaming\Postman
-set path[41]=AppData\Roaming\powershell
-set path[42]=AppData\Roaming\qBittorrent
-set path[43]=AppData\Roaming\ScreenToGif
-set path[44]=AppData\Roaming\Spotify
-set path[45]=AppData\Roaming\Subversion
-set path[46]=AppData\Roaming\TeamViewer
-set path[47]=AppData\Roaming\Telegram Desktop
-set path[48]=AppData\Roaming\TortoiseSVN
-set path[49]=AppData\Roaming\Typora
-set path[50]=AppData\Roaming\Udeler
-set path[51]=AppData\Roaming\vlc
-set path[52]=AppData\Roaming\WhatsApp
-set path[53]=AppData\Roaming\WizTree3
+set path[19]=AppData\Local\Packages\19282JackieLiu.Notepads-Beta_echhpq9pdbte8
+set path[20]=AppData\Local\Packages\33823Nicke.ScreenToGif_99xjgbc30gqtw
+set path[21]=AppData\Local\Packages\60145ScottBrogden.ditto-cp_n6b029mg40na2
+set path[22]=AppData\Local\Packages\JohannesTscholl.Pantherbar_3hp4skfxf5x2g
+set path[23]=AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
+set path[24]=AppData\Local\qBittorrent
+set path[25]=AppData\Local\TeamViewer
+set path[26]=AppData\Roaming\Apowersoft\GitMind
+set path[27]=AppData\Roaming\Code
+set path[28]=AppData\Roaming\DBeaverData
+set path[29]=AppData\Roaming\Docker
+set path[30]=AppData\Roaming\Docker Desktop
+set path[31]=AppData\Roaming\espanso
+set path[32]=AppData\Roaming\flameshot
+set path[33]=AppData\Roaming\FileZilla
+set path[34]=AppData\Roaming\Google
+set path[35]=AppData\Roaming\Insomnia
+set path[36]=AppData\Roaming\JAM Software
+set path[37]=AppData\Roaming\LGHUB
+set path[38]=AppData\Roaming\Logishrd
+set path[39]=AppData\Roaming\Macro Deck
+set path[40]=AppData\Roaming\Microsoft\Windows\PowerShell
+set path[41]=AppData\Roaming\NetBeans
+set path[42]=AppData\Roaming\obs-studio
+set path[43]=AppData\Roaming\PLSQL Developer
+set path[44]=AppData\Roaming\Postman
+set path[45]=AppData\Roaming\powershell
+set path[46]=AppData\Roaming\qBittorrent
+set path[47]=AppData\Roaming\ScreenToGif
+set path[48]=AppData\Roaming\Spotify
+set path[49]=AppData\Roaming\Subversion
+set path[50]=AppData\Roaming\TeamViewer
+set path[51]=AppData\Roaming\Telegram Desktop
+set path[52]=AppData\Roaming\TortoiseSVN
+set path[53]=AppData\Roaming\Typora
+set path[54]=AppData\Roaming\Udeler
+set path[55]=AppData\Roaming\vlc
+set path[56]=AppData\Roaming\WhatsApp
+set path[57]=AppData\Roaming\WizTree3
 
-for /l %%p in (0,1,53) do (
-   Robocopy.exe "%srcPath%\!path[%%p]!" "%dstPath%\!path[%%p]!" %params% /XD "C:\Users\Ricardo\AppData\Local\Google\AndroidStudio2021.1\tmp" "C:\Users\Ricardo\AppData\Local\Google\AndroidStudio2020.3\tmp" "C:\Users\Ricardo\AppData\Local\Google\Chrome\User Data\Default\Code" "C:\Users\Ricardo\AppData\Local\Google\Chrome\User Data\Default\Code Cache" "C:\Users\Ricardo\AppData\Local\Google\Chrome\User Data\Default\Service Worker"
+for /l %%p in (0,1,57) do (
+  echo  ❯❯❯ Copiando !path[%%p]!...
+
+  Robocopy.exe "%srcPath%\!path[%%p]!" "%dstPath%\!path[%%p]!" %params% /XD "C:\Users\Ricardo\AppData\Local\Google\AndroidStudio2021.1\tmp" "C:\Users\Ricardo\AppData\Local\Google\AndroidStudio2020.3\tmp" "C:\Users\Ricardo\AppData\Local\Google\Chrome\User Data\Default\Code" "C:\Users\Ricardo\AppData\Local\Google\Chrome\User Data\Default\Code Cache" "C:\Users\Ricardo\AppData\Local\Google\Chrome\User Data\Default\Service Worker"
 )
 
 Robocopy.exe "%srcPath%" "%dstPath%" .gitconfig .ps_history .wslconfig .yarnrc /LOG+:robocopy-log.txt
---
+++ backup-tools-hd.bat
@@ -0,0 +1,14 @@
+@echo off
+
+@REM set params=/MIR /MT /W:15 /R:7 /NS /NC /NFL /NDL /NP /LOG+:robocopy-log.txt
+
+set params=/MIR /MT /W:10 /R:3 /NP /LOG+:robocopy-log.txt
+
+set userDirectory=Ricardo
+
+set srcPath=C:\tools
+set dstPath=E:\Ricardo\tools
+
+del "C:\tools\robocopy-scripts\robocopy-log.txt"
+
+Robocopy.exe "%srcPath%" "%dstPath%" %params%
