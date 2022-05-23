a4141334c41015e381552ba220ef62c54a08ffb5
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2022-05-23 21:45:46 +0100

    Updated scripts.

--- backup-to-hd.bat
+++ backup-to-hd.bat
@@ -31,54 +31,56 @@ set path[15]=AppData\Local\Microsoft\Credentials
 set path[16]=AppData\Local\Microsoft\Windows\Cursors
 set path[17]=AppData\Local\Microsoft\Windows\Fonts
 set path[18]=AppData\Local\Movavi
-set path[19]=AppData\Local\oh-my-posh
-set path[20]=AppData\Local\Packages\19282JackieLiu.Notepads-Beta_echhpq9pdbte8
-set path[21]=AppData\Local\Packages\33823Nicke.ScreenToGif_99xjgbc30gqtw
-set path[22]=AppData\Local\Packages\60145ScottBrogden.ditto-cp_n6b029mg40na2
-set path[23]=AppData\Local\Packages\JohannesTscholl.Pantherbar_3hp4skfxf5x2g
-set path[24]=AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
-set path[25]=AppData\Local\qBittorrent
-set path[26]=AppData\Local\TeamViewer
-set path[27]=AppData\Roaming\Apowersoft\GitMind
-set path[28]=AppData\Roaming\audacity
-set path[29]=AppData\Roaming\Code
-set path[30]=AppData\Roaming\DBeaverData
-set path[31]=AppData\Roaming\Docker
-set path[32]=AppData\Roaming\Docker Desktop
-set path[33]=AppData\Roaming\espanso
-set path[34]=AppData\Roaming\flameshot
-set path[35]=AppData\Roaming\FileZilla
-set path[36]=AppData\Roaming\Google
-set path[37]=AppData\Roaming\Insomnia
-set path[38]=AppData\Roaming\JAM Software
-set path[39]=AppData\Roaming\LGHUB
-set path[40]=AppData\Roaming\Logishrd
-set path[41]=AppData\Roaming\Macro Deck
-set path[42]=AppData\Roaming\Microsoft\Windows\PowerShell
-set path[43]=AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Apps do Chrome
-set path[44]=AppData\Roaming\NetBeans
-set path[45]=AppData\Roaming\obs-studio
-set path[46]=AppData\Roaming\PLSQL Developer
-set path[47]=AppData\Roaming\Postman
-set path[48]=AppData\Roaming\powershell
-set path[49]=AppData\Roaming\qBittorrent
-set path[50]=AppData\Roaming\ScreenToGif
-set path[51]=AppData\Roaming\Spotify
-set path[52]=AppData\Roaming\Subversion
-set path[53]=AppData\Roaming\TeamViewer
-set path[54]=AppData\Roaming\Telegram Desktop
-set path[55]=AppData\Roaming\TortoiseSVN
-set path[56]=AppData\Roaming\Typora
-set path[57]=AppData\Roaming\Udeler
-set path[58]=AppData\Roaming\vlc
-set path[59]=AppData\Roaming\WhatsApp
-set path[60]=AppData\Roaming\WizTree3
-set path[61]=AppData\Roaming\VOVSOFT
+set path[19]=AppData\Local\Mozilla
+set path[20]=AppData\Local\oh-my-posh
+set path[21]=AppData\Local\Packages\19282JackieLiu.Notepads-Beta_echhpq9pdbte8
+set path[22]=AppData\Local\Packages\33823Nicke.ScreenToGif_99xjgbc30gqtw
+set path[23]=AppData\Local\Packages\60145ScottBrogden.ditto-cp_n6b029mg40na2
+set path[24]=AppData\Local\Packages\JohannesTscholl.Pantherbar_3hp4skfxf5x2g
+set path[25]=AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState
+set path[26]=AppData\Local\qBittorrent
+set path[27]=AppData\Local\TeamViewer
+set path[28]=AppData\Roaming\Apowersoft\GitMind
+set path[29]=AppData\Roaming\audacity
+set path[30]=AppData\Roaming\Code
+set path[31]=AppData\Roaming\DBeaverData
+set path[32]=AppData\Roaming\Docker
+set path[33]=AppData\Roaming\Docker Desktop
+set path[34]=AppData\Roaming\espanso
+set path[35]=AppData\Roaming\flameshot
+set path[36]=AppData\Roaming\FileZilla
+set path[37]=AppData\Roaming\Google
+set path[38]=AppData\Roaming\Insomnia
+set path[39]=AppData\Roaming\JAM Software
+set path[40]=AppData\Roaming\LGHUB
+set path[41]=AppData\Roaming\Logishrd
+set path[42]=AppData\Roaming\Macro Deck
+set path[43]=AppData\Roaming\Microsoft\Windows\PowerShell
+set path[44]=AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Apps do Chrome
+set path[45]=AppData\Roaming\Mozilla
+set path[46]=AppData\Roaming\NetBeans
+set path[47]=AppData\Roaming\obs-studio
+set path[48]=AppData\Roaming\PLSQL Developer
+set path[49]=AppData\Roaming\Postman
+set path[50]=AppData\Roaming\powershell
+set path[51]=AppData\Roaming\qBittorrent
+set path[52]=AppData\Roaming\ScreenToGif
+set path[53]=AppData\Roaming\Spotify
+set path[54]=AppData\Roaming\Subversion
+set path[55]=AppData\Roaming\TeamViewer
+set path[56]=AppData\Roaming\Telegram Desktop
+set path[57]=AppData\Roaming\TortoiseSVN
+set path[58]=AppData\Roaming\Typora
+set path[59]=AppData\Roaming\Udeler
+set path[60]=AppData\Roaming\vlc
+set path[61]=AppData\Roaming\WhatsApp
+set path[62]=AppData\Roaming\WizTree3
+set path[63]=AppData\Roaming\VOVSOFT
 
-for /l %%p in (0,1,61) do (
+for /l %%p in (0,1,63) do (
   echo  - Copiando !path[%%p]!...
 
-  Robocopy.exe "%srcPath%\!path[%%p]!" "%dstPath%\!path[%%p]!" %params% /XD "C:\Users\Ricardo\AppData\Local\Google\AndroidStudio2021.1\tmp" "C:\Users\Ricardo\AppData\Local\Google\AndroidStudio2020.3\tmp" "C:\Users\Ricardo\AppData\Local\Google\Chrome\User Data\Default\Code" "C:\Users\Ricardo\AppData\Local\Google\Chrome\User Data\Default\Code Cache" "C:\Users\Ricardo\AppData\Local\Google\Chrome\User Data\Default\Service Worker" "C:\Users\Ricardo\AppData\Roaming\Macro Deck\credentials"
+  Robocopy.exe "%srcPath%\!path[%%p]!" "%dstPath%\!path[%%p]!" %params% /XD "C:\Users\Ricardo\AppData\Local\Google\AndroidStudio2021.1\tmp" "C:\Users\Ricardo\AppData\Local\Google\AndroidStudio2020.3\tmp" "C:\Users\Ricardo\AppData\Local\Google\Chrome\User Data\Default\Code" "C:\Users\Ricardo\AppData\Local\Google\Chrome\User Data\Default\Code Cache" "C:\Users\Ricardo\AppData\Local\Google\Chrome\User Data\Default\Service Worker" "C:\Users\Ricardo\AppData\Roaming\Macro Deck\credentials" "C:\Users\Ricardo\AppData\Local\Mozilla\Firefox\Profiles\1f1inxz9.default-release\cache2"
 )
 
 Robocopy.exe "%srcPath%" "%dstPath%" .gitconfig .ps_history .wslconfig .yarnrc /LOG+:robocopy-log.txt
