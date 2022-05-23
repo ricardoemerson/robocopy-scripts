fe62d45fcfd43b91507cc905f3bf1605218ff540
Ricardo Emerson <ricardo_emerson@yahoo.com.br>
2022-05-23 18:13:10 +0100

    Updated scripts.

--- backup-videos-hd.bat
+++ backup-videos-hd.bat
@@ -2,7 +2,7 @@
 
 @REM set params=/MIR /MT /W:15 /R:7 /NS /NC /NFL /NDL /NP /LOG+:robocopy-log.txt
 
-set params=/E /MT /W:10 /R:3 /NP /LOG+:robocopy-log.txt
+set params=/MIR /MT /W:10 /R:3 /NP /LOG+:robocopy-log.txt
 
 set userDirectory=Ricardo
 
@@ -18,5 +18,5 @@ set path[1]=Obs Studio
 for /l %%p in (0,1,1) do (
   echo  - Copiando !path[%%p]!...
 
-  Robocopy.exe "%srcPath%\!path[%%p]!" "%dstPath%\!path[%%p]!" %params%
+  Robocopy.exe "%srcPath%\!path[%%p]!" "%dstPath%\!path[%%p]!" %params% /XD "D:\Videos\Canal Youtube\Projetos\Gravacoes"
 )
